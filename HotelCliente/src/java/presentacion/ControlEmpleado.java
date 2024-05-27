51e842d2211e62ed5a5b8f127463e6ed2288b9de
JK51-DEV <168320026+JK51-DEV@users.noreply.github.com>
2024-05-27 01:44:35 -0500

    vistas empleado

--- HotelCliente/src/java/presentacion/ControlEmpleado.java
+++ HotelCliente/src/java/presentacion/ControlEmpleado.java
@@ -11,13 +11,17 @@ import javax.servlet.http.HttpServletResponse;
 import javax.xml.ws.WebServiceRef;
 import servicio.Servicios;
 import servicio.ServiciosCliente;
+import servicio.ServiciosEmpleado;
+import servicio.ServiciosEmpleado_Service;
 
-@WebServlet(name = "Control", urlPatterns = {"/Control"})
-public class Control extends HttpServlet {
+@WebServlet(name = "ControlEmpleado", urlPatterns = {"/ControlEmpleado"})
+public class ControlEmpleado extends HttpServlet {
+
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/HotelServicios/ServiciosEmpleado.wsdl")
+    private ServiciosEmpleado_Service service_1;
 
     @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/HotelServicios/Servicios.wsdl")
     private Servicios service;
-
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -27,14 +31,14 @@ public class Control extends HttpServlet {
         try {
             String dni = request.getParameter("dni");
             String nom = request.getParameter("nom");
-            Servicios server = new Servicios();
-            ServiciosCliente port = server.getServiciosClientePort();
-            if (port.grabarCliente(dni, nom)) {
+            ServiciosEmpleado_Service server = new ServiciosEmpleado_Service();
+            ServiciosEmpleado port = server.getServiciosEmpleadoPort();
+            if (port.grabarEmpleado(dni, nom)) {
                 res = "Registro Completo";
             }
             out.println(res);
         } catch (Exception e) {
-            res = "Error: "+ e;
+            res = "Error: " + e;
             out.print("Ingresar datos correctos");
         }
 
@@ -88,8 +92,11 @@ public class Control extends HttpServlet {
         return port.buscarCliente(dni);
     }
 
-    
-
-   
+    private String buscarEmpleado(java.lang.String dni) {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        servicio.ServiciosEmpleado port = service_1.getServiciosEmpleadoPort();
+        return port.buscarEmpleado(dni);
+    }
 
 }
