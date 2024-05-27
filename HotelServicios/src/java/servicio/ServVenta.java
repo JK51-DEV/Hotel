54acc7a74619a4fe8e49d4e59dfd6ee7190470db
palominodiazd <palominodiazd@gmail.com>
2024-05-26 23:07:30 -0500

    Servicio Venta
    Servicio Venta

---
+++ HotelServicios/src/java/servicio/ServVenta.java
@@ -0,0 +1,25 @@
+package servicio;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+@WebService(serviceName = "ServVenta")
+public class ServVenta {
+    
+    private Venta ven;
+    @WebMethod(operationName = "nuevaVenta")
+    public Venta nuevaVenta() {
+        ven = new Venta();
+        ven.setNum(DaoVenta.getNumGenerado());
+        ven.setFec(Fecha.getFec());
+        ven.setHor(Fecha.getHor());
+        return ven;
+    }
+
+    @WebMethod(operationName = "grabarVenta")
+    public String grabarVenta(@WebParam(name = "dni") String dni, @WebParam(name = "cod") String cod, @WebParam(name = "tot") double tot) {
+        
+        return null;
+    }
+}
---
+++ HotelServicios/src/java/servicio/Venta.java
@@ -0,0 +1,69 @@
+package servicio;
+
+public class Venta {
+    private String num, fec, hor;
+    private String dni, cod;
+    private double tot;
+
+    public Venta() {
+    }
+
+    public Venta(String num, String fec, String hor, String dni, String cod, double tot) {
+        this.num = num;
+        this.fec = fec;
+        this.hor = hor;
+        this.dni = dni;
+        this.cod = cod;
+        this.tot = tot;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getFec() {
+        return fec;
+    }
+
+    public void setFec(String fec) {
+        this.fec = fec;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getCod() {
+        return cod;
+    }
+
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+
+    public double getTot() {
+        return tot;
+    }
+
+    public void setTot(double tot) {
+        this.tot = tot;
+    }
+
+    public String getHor() {
+        return hor;
+    }
+
+    public void setHor(String hor) {
+        this.hor = hor;
+    }
+    
+    
+}
